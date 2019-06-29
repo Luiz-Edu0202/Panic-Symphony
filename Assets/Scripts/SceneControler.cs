556fc2e21407319ab9a06e25435176b08230dd63
Luiz Eduardo <Brayner.luizeduardo@gmail.com>
2019-06-29 14:53:15 -0300

    Panic Symphony 0.5

---
+++ Assets/Scripts/SceneControler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControler : MonoBehaviour
+{
+    public void Click_On_Play()
+    {
+        SceneManager.LoadScene("GameSettings");
+    }
+}
