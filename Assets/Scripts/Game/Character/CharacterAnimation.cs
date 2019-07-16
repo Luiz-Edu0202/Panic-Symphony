d162365558ccb1d474b80d0033e48d7bdb0087ea
Luiz Eduardo <Brayner.luizeduardo@gmail.com>
2019-07-16 16:58:34 -0300

    Panic Symphony 4.2

--- Assets/Scripts/Game/Character/CharacterAnimation.cs
+++ Assets/Scripts/Game/Character/CharacterAnimation.cs
@@ -5,17 +5,155 @@ using UnityEngine;
 public class CharacterAnimation : MonoBehaviour
 {
 
+    [SerializeField] private Player Player;
+    [SerializeField] private Characters Character;
+    public Animator PlayerAnimator;
+    public Animator BulletAnimator;
+    public RuntimeAnimatorController BulletControlerAnimation;
+    public RuntimeAnimatorController EugeneAnimation;
+    public RuntimeAnimatorController MauriceAnimation;
+    public RuntimeAnimatorController ChuthuluAnimation;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerAnimator = GetComponent<Animator>();
+        Player = GetComponent<CharacterIdentity>().WhoIsThisPlayer;
+        Character = this.gameObject.GetComponent<CharacterIdentity>().WhoCharacterHeChoice;
+        CharacterAnimationChoicer();
+        BulletAnimation();
     }
 
-    // Update is called once per frame
+   
+
+    void CharacterAnimationChoicer()
+    {
+        if(Character == Characters.Eugene)
+        {
+            PlayerAnimator.runtimeAnimatorController = EugeneAnimation;
+        }
+        else if(Character == Characters.Cthulhu)
+        {
+            PlayerAnimator.runtimeAnimatorController = ChuthuluAnimation;
+        }
+        else if(Character == Characters.Maurice)
+        {
+            PlayerAnimator.runtimeAnimatorController = MauriceAnimation;
+        }
+    }
     void Update()
     {
-        
+        MovimentateAnimation();
+    }
+    void BulletAnimation()
+    {
+        if(Character == Characters.Eugene)
+        {   
+            BulletAnimator.SetBool("Eugene", true);
+            BulletAnimator.SetBool("Chuthulu", false);
+            BulletAnimator.SetBool("Maurice", false);
+        }
+        else if(Character == Characters.Cthulhu)
+        {
+            BulletAnimator.SetBool("Eugene", false);
+            BulletAnimator.SetBool("Chuthulu", true);
+            BulletAnimator.SetBool("Maurice", false);
+        }
+        else if(Character == Characters.Maurice)
+        {
+            BulletAnimator.SetBool("Eugene", false);
+            BulletAnimator.SetBool("Chuthulu", false);
+            BulletAnimator.SetBool("Maurice", true);
+        }
+    }
+    void MovimentateAnimation()
+    {
+        if(Player == Player.Player1)
+        {
+            if(Input.GetAxisRaw("Player1AD") > 0)
+            {
+                    PlayerAnimator.SetBool("Up", false);
+                    PlayerAnimator.SetBool("Down", false);
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("Rigth",true );
+                    PlayerAnimator.SetBool("Idle", false);
+            }
+            else if(Input.GetAxisRaw("Player1AD") < 0)
+            {
+                    PlayerAnimator.SetBool("Up", false);
+                    PlayerAnimator.SetBool("Down", false);
+                    PlayerAnimator.SetBool("Left", true);
+                    PlayerAnimator.SetBool("Rigth", false);
+                    PlayerAnimator.SetBool("Idle", false);
+            }
+            else if(Input.GetAxisRaw("Player1SW") > 0)
+            {
+                    PlayerAnimator.SetBool("Up", true);
+                    PlayerAnimator.SetBool("Down", false);
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("Rigth", false);
+                    PlayerAnimator.SetBool("Idle", false);
+            }
+            else if(Input.GetAxisRaw("Player1SW") < 0)
+            {
+                    PlayerAnimator.SetBool("Up", false);
+                    PlayerAnimator.SetBool("Down", true);
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("Rigth", false);
+                    PlayerAnimator.SetBool("Idle", false);
+            }
+            else if(Input.GetAxisRaw("Player1SW") == 0 && Input.GetAxisRaw("Player1AD") == 0)
+            {
+                    PlayerAnimator.SetBool("Up", false);
+                    PlayerAnimator.SetBool("Down", false);
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("Rigth", false);
+                    PlayerAnimator.SetBool("Idle", true);
+            }
+        }
+        else if(Player == Player.Player2)
+        {
+                if(Input.GetAxisRaw("Player2LR") > 0)
+            {
+                    PlayerAnimator.SetBool("Up", false);
+                    PlayerAnimator.SetBool("Down", false);
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("Rigth", true);
+                    PlayerAnimator.SetBool("Idle", false);
+            }
+            else if(Input.GetAxisRaw("Player2LR") < 0)
+            {
+                    PlayerAnimator.SetBool("Up", false);
+                    PlayerAnimator.SetBool("Down", false);
+                    PlayerAnimator.SetBool("Left", true);
+                    PlayerAnimator.SetBool("Rigth", false);
+                    PlayerAnimator.SetBool("Idle", false);
+            }
+            else if(Input.GetAxisRaw("PlayerDU") > 0)
+            {
+                    PlayerAnimator.SetBool("Up", true);
+                    PlayerAnimator.SetBool("Down", false);
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("Rigth", false);
+                    PlayerAnimator.SetBool("Idle", false);
+            }
+            else if(Input.GetAxisRaw("PlayerDU") < 0)
+            {
+                    PlayerAnimator.SetBool("Up", false);
+                    PlayerAnimator.SetBool("Down", true);
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("Rigth", false);
+                    PlayerAnimator.SetBool("Idle", false);
+            }
+            else if(Input.GetAxisRaw("PlayerDU") == 0 && Input.GetAxisRaw("Player2LR") == 0)
+            {
+                    PlayerAnimator.SetBool("Up", false);
+                    PlayerAnimator.SetBool("Down", false);
+                    PlayerAnimator.SetBool("Left", false);
+                    PlayerAnimator.SetBool("Rigth", false);
+                    PlayerAnimator.SetBool("Idle", true);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Game/Match/ImageCharacterControler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageCharacterControler : MonoBehaviour
+{
+    public GameObject ThisPlayer;
+    [SerializeField] private Characters WhoIsThisCharacters;
+    public Sprite EugeneImageCharacter;
+    public Sprite ChuthuluImageCharacter;
+    public Sprite MauriceImageCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+        if(this.gameObject.name == "ImageOfPlayer1")
+        {
+            ThisPlayer = GameObject.FindGameObjectWithTag("Player 1");
+        }
+        else if(this.gameObject.name == "ImageOfPlayer2")
+        {
+            ThisPlayer = GameObject.FindGameObjectWithTag("Player 2");
+        }
+        
+
+        
+        if(ThisPlayer.GetComponent<CharacterIdentity>().WhoCharacterHeChoice == Characters.Eugene)
+        {
+            GetComponent<Image>().sprite = EugeneImageCharacter;
+        }
+        else if(ThisPlayer.GetComponent<CharacterIdentity>().WhoCharacterHeChoice == Characters.Cthulhu)
+        {
+            GetComponent<Image>().sprite = ChuthuluImageCharacter;
+        }
+        else if(ThisPlayer.GetComponent<CharacterIdentity>().WhoCharacterHeChoice == Characters.Maurice)
+        {
+            GetComponent<Image>().sprite = MauriceImageCharacter;
+        }
+    }
+}
